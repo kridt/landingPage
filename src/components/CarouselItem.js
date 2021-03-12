54cc9a930e82295ee48d8e0735c2fab3c7464b80
Christian <chrnielsen2003@gmail.com>
2021-03-12 09:39:01 +0100

    asd

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -5,7 +5,7 @@ export default function CarouselItem({ image, copy, ctaLink, ctaCopy}) {
     return(
         
                 <div className="carouselItem">
-                    <img className="carouselItem__image" src={image} />
+                    <img alt="" className="carouselItem__image" src={image} />
                     <div className="carouselItem__content">
                         <p className="carouselItem__copy">{copy}</p>
                         <a href={ctaLink} className="carouselItem__CTA">{ctaCopy}</a>
